9ff1db5e2b01a224aec55d22265da96f3f0945cf
Lautaro <n.alegria@outlook.com>
2021-03-31 13:12:30 -0300

    Trabajo Practico

--- TP/BACKEND/mostrar.php
+++ TP/BACKEND/mostrar.php
@@ -7,9 +7,7 @@ $modo = "r";
 
 $archivo = fopen($path, $modo);
 
-?>
-
-<!DOCTYPE html>
+echo "<!DOCTYPE html>
 <html lang=\"en\">
 <head>
     <meta charset=\"UTF-8\">
@@ -17,7 +15,10 @@ $archivo = fopen($path, $modo);
 </head>
 <body>
 <h2>Mostrar</h2>
-<?php
+<table align=\"center\">
+    <tr><td>Listado de empleados</td></tr>
+    <tr><td colspan='10'><hr></td></tr>
+";
 
 while(!feof($archivo)){
     // 'trim()' -> elimina espacios en blanco al inicio y final de la cadena
@@ -27,14 +28,14 @@ while(!feof($archivo)){
         // Con 'explode()' divido una cadena de acuerdo a un seaprador pasado por parametro, retorna un array
         $arrayEmpleado = explode(" - ", $unEmpleado);
         $nuevoEmpleado = new Empleado($arrayEmpleado[0], $arrayEmpleado[1], $arrayEmpleado[2], $arrayEmpleado[3],$arrayEmpleado[4], $arrayEmpleado[5], $arrayEmpleado[6]);
-        echo $nuevoEmpleado->ToString() . "<br/>";
+        echo "<tr><td>".$nuevoEmpleado->ToString()."</td>";
+        echo "<td><a href='eliminar.php?legajo=" . $nuevoEmpleado->GetLegajo() . "'>Eliminar</a></td></tr>";
     }
 }
 fclose($archivo);
-echo "<br/>";
-echo "<a href=\"../FRONTEND/index.html\">Alta Empleado</a>";
+echo "<tr><td align=right><a href=\"../FRONTEND/index.html\">Alta Empleado</a></td></tr>";
 
-?>
+echo "
+</table>
 </body>
-</html>
-
+</html>";
